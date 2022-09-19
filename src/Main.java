8d8c71aa5ff6c5039b78af675fd0592625507432
4tradejimi <4tradejimi@proton.me>
2022-09-19 18:29:54 +0200

    added to run from main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Solution.solution("balloonbloonnballoonballoonballoon"));
-
+        System.out.println(Solution.solution2("BALLONNBALLONNBALLONNBALLONNBALLONNBALLONNBALLONN"));
         System.out.println("Hello world!");
     }
 }
